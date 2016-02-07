4e42c80da97c60957ccfca0fd4f1dc422e0a0737
Craig Anderson <craig.anderson@talktalkplc.com>
2016-02-07 20:05:54 +0000

    Added a gulp task to build the app and push to github pages

---
+++ gulpFile.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Craig on 07/02/2016.
+ */
+
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var rename = require('gulp-rename');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('dist/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('default', function() {
+  var bundle = gulp.src('app/main.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest('dist/'));
+
+  var html = gulp.src('app/index.html')
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest('dist/'));
+
+  return Promise.all([bundle, html]);
+});
